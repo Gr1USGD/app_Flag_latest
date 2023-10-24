718d2bac2d69ea45448781abad9dd299e3e909a5
Gr1USGD <uyen.mt40156@sinhvien.hoasen.edu.vn>
2023-10-24 08:32:46 +0700

    fixconnect

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet,Text,View } from 'react-native';
+import React from 'react'
+import Home from './src/screens/Home/home'
 
-export default function App() {
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Home/>
+    
+      <StatusBar style="auto" /></View>
+
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    background: 'FaFbFa',
     justifyContent: 'center',
   },
-});
+})
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import { Image, Dimensions, View, Text, StyleSheet } from 'react-native';
 import React from 'react'
-
+const image = { uri: 'https://i.pinimg.com/564x/1f/8b/34/1f8b34a81ded531546dda85c1dd45856.jpg' };
 const Home = () => {
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <View style={style.body}>
+      <View style = {{ }}>
+        <Image source={image} resizeMode="cover" style={style.image}>
+        </Image>
+      </View>
+      <Text style={style.text}>Touch to screens</Text>
+    </View >
   )
 }
 
 export default Home
+const style = StyleSheet.create({
+  body: {
+    flex:1,
+    width: '100%',
+    height: '100%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    width: Dimensions.get('screen').width,
+  },
+  text: {
+    width: 350,
+    height: 50,
+    position: 'absolute',
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 500,
+    justifyContent: 'center',
+  }
+});
